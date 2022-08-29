670b590f510c46e39997a1f47cd2f84400f12416
Sam Schultz <sam.schultz@outlook.com>
2022-08-29 09:01:40 -0400

    Add project files.

---
+++ Restraunt Picker/Program.cs
@@ -0,0 +1,32 @@
+﻿
+String[] Restraunts = { "Mcdonald's", "Chiptole", "Chic Fil A", "Burger King", "Ihop", "Noodles and Co", "Cloud 9", "Taco Bell", "Skyline/Gold Star", "Qdoba"};
+
+string response = "Waiting";
+ 
+Random rd = new Random();
+
+int rand_num = rd.Next(0,10);
+
+Console.WriteLine(Restraunts[rand_num]);
+Console.WriteLine("Is this the restraunt you'd like to eat at? y or n");
+response = Console.ReadLine();
+
+if (response == "y")
+{
+    Console.WriteLine("Enjoy your meal!");
+
+}
+
+while (response == "n")
+{
+    rand_num = rd.Next(0, 10);
+    Console.WriteLine(Restraunts[rand_num]);
+    Console.WriteLine("Is this the restraunt you'd like to eat at? Y or N");
+    response = Console.ReadLine();
+}
+
+if (response == "y")
+{
+    Console.WriteLine(" Enjoy your meal!");
+
+}
